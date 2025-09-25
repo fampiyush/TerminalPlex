b5ddc6cf619bfaabbc35568032f22e60a3b56f49
Piyush Gupta <piyushgupta941d@gmail.com>
2025-09-25 20:32:25 +0530

    updates openai package

--- terminalplex.py
+++ terminalplex.py
@@ -27,6 +27,7 @@ Provide a relevant, informative response to the user's query using the given con
 - Answer directly without referring the user to any external links.
 - Use an unbiased, journalistic tone and avoid repeating text.
 - Cite all information using [citation number] notation of the website link (dont't write the word citation only the number like [1]), matching each part of your answer to its source from the context block given below.
+- Keep the total answer length within 100 words.
 
 Context Block:
 {context_block}
@@ -41,7 +42,7 @@ user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 async def search_ddg(query):
     loop = asyncio.get_event_loop()
     try:
-        search_results = await loop.run_in_executor(None, lambda: list(ddgs.text(query, max_results=4)))
+        search_results = await loop.run_in_executor(None, lambda: list(ddgs.text(query, max_results=3)))
         urls = [result['href'] for result in search_results if result and 'href' in result]
         return urls
     except Exception as e:
@@ -64,7 +65,7 @@ async def extract_text_from_url(url):
         return None
     soup = await loop.run_in_executor(None, lambda: BeautifulSoup(response.text, "html.parser"))
     text = soup.get_text()
-    return text[:10000]
+    return text[:1000]
 
 async def get_all_text_from_urls(query):
     text = ""
@@ -86,37 +87,34 @@ def get_search_query(query, prev_messages, client):
     query = search_prompt.format(query=query)
     prev_messages = prev_messages or []
     new_messages = prev_messages + [{"role": "user", "content": query}]
-    response = client.chat.completions.create(
+    response = client.responses.create(
         model=os.getenv("MODEL_NAME"),
-        messages=[{"role": "system", "content": system_prompt_search}, *new_messages],
-        max_tokens=50,
+        input=[{"role": "system", "content": system_prompt_search}, *new_messages],
+        max_output_tokens=500,
     )
 
-    print(f"Search Query: {response.choices[0].message.content}\n")
-    return response.choices[0].message.content
+    print(f"Search Query: {response}\n")
+    return response.output[1].content[0].text
 
 def get_cited_answer(query, context_block, prev_messages, client):
     query = cited_answer_prompt.format(query=query, context_block=context_block)
     prev_messages = prev_messages or []
     new_messages = prev_messages + [{"role": "user", "content": query}]
-    full_answer = ""
-    response = client.chat.completions.create(
+    response = client.responses.create(
         model=os.getenv("MODEL_NAME"),
-        messages=[{"role": "system", "content": system_prompt_cited_answer}, *new_messages],
-        max_tokens=int(os.getenv("MAX_TOKENS")),
-        stream=True
+        input=[{"role": "system", "content": system_prompt_cited_answer}, *new_messages],
+        max_output_tokens=int(os.getenv("MAX_TOKENS")),
     )
+    print(f"Full Response: {response}\n")
+    answer = response.output[1].content[0].text
 
-    #Streamnig the response
     print("-"*40)
     print("Answer: \n")
-    for chunk in response:
-        full_answer += chunk.choices[0].delta.content if chunk.choices[0].delta.content else ""
-        print(chunk.choices[0].delta.content, end="")
+    print(answer)
     print("\n")
     print("-"*40)
 
-    new_prev_messages = prev_messages + [{"role": "assistant", "content": full_answer}]
+    new_prev_messages = prev_messages + [{"role": "assistant", "content": answer}]
     return new_prev_messages
     
 
